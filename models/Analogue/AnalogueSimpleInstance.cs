2346635a21d4e4e86b2948069886f4498c8e2f8a
Matt Pannella <mattpannella@gmail.com>
2023-02-02 23:56:44 -0500

    kinda sorting working instance json builder

---
+++ models/Analogue/AnalogueSimpleInstance.cs
@@ -0,0 +1,8 @@
+namespace Analogue;
+
+public class SimpleInstance
+{
+    public InstanceDataSlot[] data_slots { get; set; }
+    public string data_path { get; set; } = "";
+    public string magic { get; set; } = "APF_VER_1";
+}
--- models/InstancePackager/InstancePackager.cs
+++ models/InstancePackager/InstancePackager.cs
@@ -4,4 +4,5 @@ public class InstancePackager
 {
     public List<DataSlot> data_slots { get; set; }
     public string output { get; set; }
+    public string platform_id { get; set; }
 }
