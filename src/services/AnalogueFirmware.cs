135882f44bc727045bb9859d6eb663105b39b7aa
Matt Pannella <mattpannella@gmail.com>
2023-05-11 22:25:07 -0400

    use new analogue firmware endpoint

--- src/services/AnalogueFirmware.cs
+++ src/services/AnalogueFirmware.cs
@@ -1,24 +1,42 @@
 using System.Text.Json;
 
 namespace pannella.analoguepocket;
+using Analogue;
 
 public static class AnalogueFirmware
 {
     private const string BASE_URL = "https://www.analogue.co/";
     private const string VERSION = "support/pocket/firmware/latest";
     private const string DOWNLOAD = "support/pocket/firmware/{0}/download";
+    private const string DETAILS = "support/pocket/firmware/{0}/details";
 
+    private static ReleaseDetails? latest = null;
 
     public static async Task<string> GetLatestVersion()
     {
-        string url = BASE_URL + VERSION;
-        string response = await Factory.GetHttpHelper().GetHTML(url, false);
-
-        return response;
+        ReleaseDetails? details = await GetDetails();
+        return details.version;
     }
 
-    public static string GetFirmwareUrl(string version = "latest")
+    public static async Task<ReleaseDetails?> GetDetails(string version = "latest")
     {
-        return String.Format(BASE_URL + DOWNLOAD, version);
+        if(latest != null) {
+            return latest;
+        }
+        string url = String.Format(BASE_URL + DETAILS, version);
+        string response = await Factory.GetHttpHelper().GetHTML(url);
+        ReleaseDetails? details = JsonSerializer.Deserialize<ReleaseDetails>(response);
+
+        if (version == "latest") {
+            latest = details;
+        }
+
+        return details;
+    }
+
+    public static async Task<string> GetFirmwareUrl(string version = "latest")
+    {
+        ReleaseDetails? details = await GetDetails(version);
+        return details.download_url;
     }
 }
