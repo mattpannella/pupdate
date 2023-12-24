11e4c229a68bdb6f73a997068ceae4dc74fc9410
Matt Pannella <mattpannella@gmail.com>
2023-12-24 14:32:39 -0500

    DOH

---
+++ src/models/Analogue/Cores/Core/AnalogueCore.cs
@@ -0,0 +1,10 @@
+namespace pannella.analoguepocket.Analogue.Cores.Core;
+
+public class Core
+{
+    public Metadata? metadata { get; set; }
+    public string? magic { get; set; }
+
+    public Framework? framework { get; set; }
+    
+}
