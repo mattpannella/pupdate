78e2d1f624d1a388e916d70583c21a3bc28e6717
Michael Hallett <michael@crazycompsci.com>
2024-02-10 18:26:30 -0500

    Updated the Instance Generator options
    brought the instance generator verb inline with the other verbs while preserving existing functionality with the old verb

--- src/options/InstanceGeneratorOptions.cs
+++ src/options/InstanceGeneratorOptions.cs
@@ -2,7 +2,7 @@ using CommandLine;
 
 namespace Pannella.Options;
 
-[Verb("instancegenerator",  HelpText = "Run the instance JSON generator")]
+[Verb("instance-generator", aliases: new[] { "instancegenerator" }, HelpText = "Run the instance JSON generator for PC Engine CD" )]
 public class InstanceGeneratorOptions
 {
     [Option('p', "path", HelpText = "Absolute path to install location", Required = false)]
