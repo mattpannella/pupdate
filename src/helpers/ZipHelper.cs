5419587126dffc6ff4c84c068ca916f42b0cc200
Matt Pannella <mattpannella@gmail.com>
2024-03-09 00:15:50 -0500

    version bump

--- src/helpers/ZipHelper.cs
+++ src/helpers/ZipHelper.cs
@@ -4,9 +4,7 @@ namespace Pannella.Helpers;
 
 public class ZipHelper
 {
-    public Progress<ZipProgress> _progress;
-
-    private static void Report(object sender, ZipProgress zipProgress)
+    private static void UpdateProgress(object sender, ZipProgress zipProgress)
     {
         var progress = (double)zipProgress.Processed / (double)zipProgress.Total;
 
@@ -29,13 +27,12 @@ public class ZipHelper
     public static void ExtractToDirectory(string zipFile, string destination, bool overwrite = false)
     {
         Progress<ZipProgress> _progress = new Progress<ZipProgress>();
-        _progress.ProgressChanged += Report;
+        _progress.ProgressChanged += UpdateProgress;
         var stream = new FileStream(zipFile, FileMode.Open);
         var zip = new ZipArchive(stream);
         zip.ExtractToDirectory(destination, _progress, overwrite);
         stream.Close();
     }
-
 }
 
 public class ZipProgress
@@ -59,27 +56,17 @@ public static class ZipExtension
     }
 
     public static void ExtractToDirectory(this ZipArchive zipFile, string target, IProgress<ZipProgress> progress, bool overwrite)
-    {
-        if (zipFile == null)
-        {
-            throw new ArgumentNullException(nameof(zipFile));
-        }
-            
-        if (target == null)
-        {
-            throw new ArgumentNullException(nameof(target));
-        }
-            
+    { 
         DirectoryInfo info = Directory.CreateDirectory(target);
-        string destinationDirectoryFullPath = info.FullName;
+        string targetPath = info.FullName;
 
         int count = 0;
         foreach (ZipArchiveEntry entry in zipFile.Entries)
         {
             count++;
-            string fileDestinationPath = Path.GetFullPath(Path.Combine(destinationDirectoryFullPath, entry.FullName));
+            string fileDestinationPath = Path.GetFullPath(Path.Combine(targetPath, entry.FullName));
 
-            if (!fileDestinationPath.StartsWith(destinationDirectoryFullPath, StringComparison.OrdinalIgnoreCase))
+            if (!fileDestinationPath.StartsWith(targetPath, StringComparison.OrdinalIgnoreCase))
             {
                 throw new IOException("File is extracting to outside of the folder specified.");
             }
