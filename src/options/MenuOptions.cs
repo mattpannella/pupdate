6c0a731a9f3eb5f0c8d0aebb92377d6d5c0c4281
Matt Pannella <mattpannella@gmail.com>
2024-04-03 21:34:59 -0400

    fixed self install in macos (#286)

--- src/options/MenuOptions.cs
+++ src/options/MenuOptions.cs
@@ -5,6 +5,8 @@ namespace Pannella.Options;
 [Verb("menu", isDefault: true, HelpText = "Interactive Main Menu")]
 public class MenuOptions : BaseOptions
 {
+    public MenuOptions() {}
+
     [Option('s', "skip-update", HelpText = "Skip the self update check", Required = false)]
     public bool SkipUpdate { get; set; }
 }
