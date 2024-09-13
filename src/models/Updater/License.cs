54cf80fd02c731479828af8f89c2d8efccf2aaf1
Matt Pannella <mattpannella@gmail.com>
2024-09-12 20:34:22 -0400

    model update

---
+++ src/models/Updater/License.cs
@@ -0,0 +1,8 @@
+using System.ComponentModel;
+
+namespace Pannella.Models.Updater;
+
+public class License
+{
+    public string filename { get; set; }
+}
--- src/models/Updater/Updaters.cs
+++ src/models/Updater/Updaters.cs
@@ -1,6 +1,9 @@
+using System.ComponentModel;
+
 namespace Pannella.Models.Updater;
 
 public class Updaters
 {
     public Substitute[] previous { get; set; }
+    public License license { get; set; }
 }
