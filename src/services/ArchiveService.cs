8be0054a36647f0a5692f5a6baceec9bce94d927
Matt Pannella <mattpannella@gmail.com>
2025-03-25 22:19:44 -0400

    fix error messages when archive is down (#405)
    * fix error messages when archive is down
    
    * not needed

--- src/services/ArchiveService.cs
+++ src/services/ArchiveService.cs
@@ -7,6 +7,7 @@ using SettingsArchive = Pannella.Models.Settings.Archive;
 using ArchiveFile = Pannella.Models.Archive.File;
 using Archive = Pannella.Models.Archive.Archive;
 using UrlCombineLib;
+using System.Linq.Expressions;
 
 namespace Pannella.Services;
 
@@ -83,6 +84,11 @@ public class ArchiveService : Base
             else
             {
                 internetArchive = ArchiveService.GetFiles(archive.archive_name);
+            }
+            if (internetArchive == null) {
+                WriteMessage($"Unable to load Assets Index for '{archive.archive_name}'");
+                internetArchive = new Archive();
+                internetArchive.files = new List<ArchiveFile>(); //empty list
             }
 
             this.archiveFiles.Add(archive.archive_name, internetArchive);
