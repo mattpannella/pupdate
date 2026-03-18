f07bb56ea07bacd5ab165ba2cea1f3e519197188
Michael Hallett <michael@rehalletter.com>
2026-03-18 17:45:28 -0400

    Formatting updates

--- src/models/OpenFPGA_Cores_Inventory/V3/Core.cs
+++ src/models/OpenFPGA_Cores_Inventory/V3/Core.cs
@@ -10,13 +10,9 @@ public class Core
     public string id { get; set; }
     public Repository repository { get; set; }
     public List<Release> releases { get; set; }
-
     public Release release { get; set; }
-
     public string platform_id { get; set; }
-
     public Platform platform { get; set; }
-
     public string download_url { get; set; }
     public string version { get; set; }
     public string release_date { get; set; }
@@ -25,15 +21,14 @@ public class Core
     public bool requires_license { get; set; }
 
     public Updaters updaters { get; set; }
-
     public string license_slot_id;
-
     public int license_slot_platform_id_index;
-
     public string license_slot_filename;
 
     public override string ToString()
     {
-        return platform?.name != null ? $"{platform.name} ({id})" : id ?? string.Empty;
+        return platform?.name != null
+            ? $"{platform.name} ({id})"
+            : id ?? "Unknown";
     }
 }
