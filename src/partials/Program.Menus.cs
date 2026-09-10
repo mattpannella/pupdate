39b0db8ff567e648262df0e6ff59cd8e09f3df82
Matt Pannella <mattpannella@gmail.com>
2026-09-09 22:01:09 -0400

    removing platform limit warnings (#522)

--- src/partials/Program.Menus.cs
+++ src/partials/Program.Menus.cs
@@ -27,8 +27,6 @@ internal static partial class Program
 
         string sponsorLinks = GetRandomSponsorLinks();
 
-        string menuCacheLine = GetMenuCacheStatusLine();
-
         var menuConfig = new MenuConfig
         {
             Selector = "=>",
@@ -39,13 +37,6 @@ internal static partial class Program
                 Console.WriteLine(welcome);
                 Console.WriteLine(sponsorLinks);
                 Console.WriteLine(rateLimitMessage);
-
-                if (!string.IsNullOrEmpty(menuCacheLine))
-                {
-                    Console.WriteLine();
-                    Console.WriteLine(menuCacheLine);
-                }
-
                 Console.WriteLine();
                 Console.WriteLine("Choose your destiny:");
             },
@@ -662,7 +653,6 @@ internal static partial class Program
                 Console.WriteLine("Starting update process...");
                 coreUpdaterService.RunUpdates();
                 ServiceHelper.CoresService.RefreshInstalledCores();
-                menuCacheLine = GetMenuCacheStatusLine();
                 Pause();
             })
             .Add("Update Firmware", _ =>
@@ -680,7 +670,6 @@ internal static partial class Program
                 // "Update All" until pupdate is restarted. See issue #299.
                 ServiceHelper.ReloadSettings();
                 coreUpdaterService.ReloadSettings();
-                menuCacheLine = GetMenuCacheStatusLine();
             })
             .Add("Download Assets", _ =>
             {
@@ -700,11 +689,7 @@ internal static partial class Program
                 Pause();
             })
             .Add("Pocket Setup            >", pocketSetupMenu.Show)
-            .Add("Pocket Maintenance      >", () =>
-            {
-                pocketMaintenanceMenu.Show();
-                menuCacheLine = GetMenuCacheStatusLine();
-            })
+            .Add("Pocket Maintenance      >", pocketMaintenanceMenu.Show)
             .Add("Pocket Extras           >", pocketExtrasMenu.Show)
             .Add("Settings                >", () =>
             {
